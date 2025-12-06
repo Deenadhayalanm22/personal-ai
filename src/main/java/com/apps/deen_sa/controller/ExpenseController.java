8b0adfb1dd84ca7ad6a7d60de67c748c36a9ac48
Deenadhayalanm22 <53555317+Deenadhayalanm22@users.noreply.github.com>
2025-12-07 01:49:48 +0530

    Update ExpenseController.java

--- src/main/java/com/apps/deen_sa/controller/ExpenseController.java
+++ src/main/java/com/apps/deen_sa/controller/ExpenseController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/expenses")
+@RequestMapping("/api/expenses")
 public class ExpenseController {
 
     private final ExpenseService expenseService;
